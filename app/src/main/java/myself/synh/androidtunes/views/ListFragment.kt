7d3a67736a5c49dd31416056f0c84423c41e58ba
NightOfHorror <nightbringer96@yandex.ru>
2020-01-23 22:47:27 +0300

    Added list fragment

---
+++ app/src/main/java/myself/synh/androidtunes/views/ListFragment.kt
@@ -0,0 +1,16 @@
+package myself.synh.androidtunes.views
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import myself.synh.androidtunes.R
+
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+
+}
